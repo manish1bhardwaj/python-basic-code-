e06d33f069e778bc9517182ffad296442a58ec69
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2023-09-03 12:34:19 +0530

    vol.of cylinder

---
+++ vol.of cylinder.py
@@ -0,0 +1,11 @@
+#volume of cylinder
+
+#input section
+r=int(input("enter the radius of cylinder"))
+h=int(input("entre the height of cylinder"))
+
+#logic section
+V=3.14*r*r*h
+
+#display section
+print("volume of cylinder is",V)
