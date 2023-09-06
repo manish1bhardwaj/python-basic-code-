46074b444a1a91809f9f4ec590e888de93f28b67
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2023-09-06 10:06:02 +0530

    area and perimeter of rect

---
+++ area and perimeter of rect..py
@@ -0,0 +1,9 @@
+#area and perimeter of the rectangle?
+l=int(input("length of rectangle"))
+b=int(input("breadth of rectangle"))
+
+P=2*(l+b)
+A=l*b
+
+print("perimeter of rectangle is",P)
+print("area of rectangle",A)
