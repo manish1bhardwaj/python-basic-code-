ca0309cf6bf67db2f5d486d7b73725cca0e29b36
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2023-09-06 10:06:58 +0530

    hypotenuse of right angle triangle

---
+++ hypotenuse of right angle triangle.py
@@ -0,0 +1,11 @@
+#hypotenuse of right angle triangle
+
+
+h=int(input("enter the height of right angle triangle"))
+b=int(input("enter the base of right angle triangle"))
+
+p= (h**2+b**2)*1/2
+
+
+
+print("hypotenuse of right angle triangle is",p)
